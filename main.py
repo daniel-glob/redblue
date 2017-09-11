37987100afbc92cf4f8892ebba03c8ca1d4c5087
daniel-glob <daniel.s@globant.com>
2017-09-11 12:59:12 -0500

    Message change v.0.2.2., background blue

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('<body style=\'background-color: green\'>Papa Johns Red Blue release V.0.2.1.!</body>')
+        self.response.write('<body style=\'background-color: blue\'>Papa Johns Red Blue release V.0.2.2.!</body>')
 
 
 app = webapp2.WSGIApplication([
